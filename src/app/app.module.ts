56529642b81084e52c38afc394a0014e8aba0e8f
Aron9013 <aron.d28@gmail.com>
2018-10-04 22:35:56 +0100

    Made the page have some stuff around the edges to see how the search bar would look integrated onto a page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {SearchBarComponent} from "./search-bar/search-bar.component";
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {PageTopComponent} from "./page-top/page-top.component";
+import {LogoComponent} from "./nav-bar/logo/logo.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    NavBarComponent,
+    PageTopComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page-top/page-top.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'page-top',
+  templateUrl: 'page-top.component.html',
+  styleUrls: ['page-top.component.scss']
+})
+
+export class PageTopComponent {
+}
